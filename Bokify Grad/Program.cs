9f483282d9f58ec82a60d2f49c23be7a05e0f999
islaam7 <bgbos7077@gmail.com>
2025-11-23 18:20:14 +0200

    Identity + EF Core registered Token providers enabled (email confirm, reset password)

--- Bokify Grad/Program.cs
+++ Bokify Grad/Program.cs
@@ -1,7 +1,13 @@
 
-using System;
-using Microsoft.EntityFrameworkCore;
 using BokifyGrad.DAL;
+using BokifyGrad.DAL.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+using System;
+
 
 namespace Bokify_Grad
 {
@@ -10,9 +16,32 @@ namespace Bokify_Grad
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
-            builder.Services.AddDbContext<AppDBContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+            builder.Services.AddDbContext<AppDBContext>(options =>    
+            options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
+            builder.Services.AddIdentity<ApplicationUser, IdentityRole>()
+              .AddEntityFrameworkStores<AppDBContext>()
+              .AddDefaultTokenProviders();
+
+            builder.Services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            })
+                .AddJwtBearer(options =>
+                {
+                    options.TokenValidationParameters = new TokenValidationParameters
+                    {
+                        ValidateIssuer = true,
+                        ValidateAudience = true,
+                        ValidateLifetime = true,
+                        ValidateIssuerSigningKey = true,
+                        ValidIssuer = builder.Configuration["Jwt:Issuer"],
+                        ValidAudience = builder.Configuration["Jwt:Audience"],
+                        IssuerSigningKey = new SymmetricSecurityKey(
+                            Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"]))
+                    };
+                });
 
             // Add services to the container.
 
--- BokifyGrad.DAL/AppDBContext.cs
+++ BokifyGrad.DAL/AppDBContext.cs
@@ -1,11 +1,12 @@
 ﻿using BokifyGrad.DAL.Models;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 
 
 
 namespace BokifyGrad.DAL
 {
-    public class AppDBContext : DbContext
+    public class AppDBContext : IdentityDbContext<ApplicationUser>
     {
         public DbSet<User> users { get; set; }
         public AppDBContext(DbContextOptions<AppDBContext> options) : base(options) { }
